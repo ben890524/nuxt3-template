de4c135ff0e257d9bf955218eff404ac960cd949
Ben Li <ben10723021@gmail.com>
2022-12-16 12:13:45 +0800

    add tailwincss and sass

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="p-0 m-0">
     <NuxtWelcome />
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-})
+  modules: ['@nuxtjs/tailwindcss'],
+  tailwindcss: {
+    configPath: '~/tailwind.config.js',
+    cssPath: '@/assets/styles/tailwind/index.scss',
+  },
+  css: ['@/assets/styles/tailwind/index.scss'],
+});
