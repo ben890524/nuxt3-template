77c995683c0b240cf90f188053724330a50c2cb2
Ben Li <ben10723021@gmail.com>
2022-12-16 12:44:36 +0800

    add error page and error layout

---
+++ error.vue
@@ -0,0 +1,32 @@
+<template>
+  <NuxtLayout name="error-layout">
+    <div class="flex flex-col items-center justify-center">
+      <p class="text-xl text-gray-500">
+        {{ error!.message }}
+      </p>
+      <button
+        type="button"
+        class="block px-4 py-2 mt-8 text-sm font-medium text-gray-700 bg-gray-100 border border-transparent rounded-md hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2"
+        @click="handleError"
+      >
+        Clear Error To Home Page
+      </button>
+    </div>
+  </NuxtLayout>
+</template>
+
+<script setup lang="ts">
+interface IError {
+  message: string;
+}
+interface IProps {
+  error?: IError;
+}
+
+withDefaults(defineProps<IProps>(), {
+  error: () => {
+    return { message: '' };
+  },
+});
+const handleError = () => clearError({ redirect: '/' });
+</script>
---
+++ layouts/ErrorLayout.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="flex flex-col items-center py-24 bg-white">
+    <h1 class="my-6 text-5xl font-semibold text-red-500">There are some Error!</h1>
+    <slot />
+  </div>
+</template>
+<script setup lang="ts">
+/* import components */
+</script>
+<style scoped lang="scss"></style>
