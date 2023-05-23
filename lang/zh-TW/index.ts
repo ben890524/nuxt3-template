7504ce61612abe9102410906b101e91d7d0abe87
Ben Li <ben10723021@gmail.com>
2023-05-23 15:38:14 +0800

    feat: import nuxtjs/i18n and settings

---
+++ lang/zh-TW/index.ts
@@ -0,0 +1,4 @@
+console.log(123);
+export default {
+  language: '繁體中文',
+};
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss'],
+  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/i18n'],
   css: ['@/assets/css/tailwind.scss'],
   plugins: ['@/plugins/antd'],
+  i18n: {
+    defaultLocale: 'zh-TW',
+    lazy: true,
+    langDir: 'lang',
+    detectBrowserLanguage: {
+      useCookie: false,
+    },
+    locales: [
+      { code: 'zh-TW', file: 'zh-TW/index.ts' },
+      { code: 'en-US', file: 'en-US/index.ts' },
+    ],
+  },
 });
