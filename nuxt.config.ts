e737a9bc2c9df28efd4191ccec8dcf45bc1b437f
Ben Li <ben10723021@gmail.com>
2023-05-23 16:39:21 +0800

    feat: import vueUse and settings

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/i18n', '@pinia/nuxt'],
+  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/i18n', '@pinia/nuxt', 'nuxt-lodash'],
   css: ['@/assets/css/tailwind.scss'],
   plugins: ['@/plugins/antd'],
   i18n: {
