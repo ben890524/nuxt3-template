7abdc2c129acf9fcb57b1515e68e9a6ee15dbf73
Ben Li <ben10723021@gmail.com>
2023-05-23 12:38:38 +0800

    [feat] import tailwindcss and settings

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,8 @@
 <template>
-  <NuxtWelcome />
+  <div>
+    <h1 class="text-gray-400 font-bold text-2xl text-center">ERP BACKEND</h1>
+    <NuxtWelcome />
+  </div>
 </template>
 <script setup lang="ts"></script>
 <style scoped lang="scss"></style>
